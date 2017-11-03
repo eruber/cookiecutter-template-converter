5b8c38bfc53a5013fd06c174fab3a3e49008a5e9
eruber <eruber@gmail.com>
2017-11-03 15:13:32 -0700

    Added classifiers list

--- setup.py
+++ setup.py
@@ -49,5 +49,6 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Pre-processors',
     ]
 )
